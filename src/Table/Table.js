5ac0317e396ac1c3e86a3e52648efd8c5e2761a6
Travis Hoki <travis.hoki@xant.ai>
2021-08-17 10:58:41 -0600

    initial code

---
+++ src/Table/Table.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+
+import {
+  getSkateboardBrandLocations,
+  getSkateboardBrands,
+} from '../api';
+
+const Table = () => {
+  const [ brands, setBrands ] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    console.log('%c---- %s -----', 'font-size: 12px;', 'fetchData');
+    Promise.all([
+      getSkateboardBrands(),
+      getSkateboardBrandLocations(),
+    ])
+    .then((values) => {
+      console.log('-- response -- ');
+      const [ brandData, brandLocationData ] = values;
+
+      console.log('brandData:');
+      console.dir(brandData);
+      setBrands(brandData);
+
+      console.log('brandLocationData:');
+      console.dir(brandLocationData);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Skate Brands</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {brands.map((item, index) => (
+            <tr key={item.id}>
+              <td>{index}</td>
+              <td>{item.name}</td>
+              <td>{item.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+const MOCK_SKATEBOARD_BRAND = [
+  {
+    id: 1,
+    name: 'Hoki Skateboards'
+  },
+  {
+    id: 2,
+    name: 'Pixie Skateboards'
+  },
+  {
+    id: 3,
+    name: 'ABF Skate Lessons'
+  },
+  {
+    id: 4,
+    name: 'Zero Skateboards'
+  },
+  {
+    id: 5,
+    name: 'Santa Cruz'
+  },
+];
+
+const MOCK_SKATEBOARD_BRAND_LOCATIONS = [
+  {
+    id: 1,
+    location: 'Utah'
+  },
+  {
+    id: 2,
+    location: 'Arizona'
+  },
+  {
+    id: 4,
+    location: 'California'
+  },
+  {
+    id: 5,
+    location: 'California'
+  },
+];
+
+export const getSkateboardBrands = () => new Promise((resolve) => {
+  resolve(MOCK_SKATEBOARD_BRAND);
+});
+
+export const getSkateboardBrandLocations = () => new Promise((resolve) => {
+  resolve(MOCK_SKATEBOARD_BRAND_LOCATIONS);
+});
